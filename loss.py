7c071adc760aa93b3b505582c47b7f4cecf42b91
Matt Macy <mmacy@nextbsd.org>
2017-03-09 15:15:05 -0800

    fix up backward() and add dice_loss() function

--- loss.py
+++ loss.py
@@ -1,16 +1,20 @@
 from torch.autograd import Function
 
 
-
+# Intersection = dot(A, B)
+# Union = dot(A, A) + dot(B, B)
 # The Dice loss function is defined as
 # 1/2 * intersection / union
+#
+# The derivative is 2[(union * target - 2 * intersect * input) / union^2]
 
 class DiceLoss(Function):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         pass
 
     def forward(self, input, target):
         self.save_for_backward(input, target)
+        # target is a binary valued tensor - dot(target, target) => sum(target)
         out = 2*torch.dot(input, target) / (torch.dot(input, input) +  torch.sum(target))
         return out
 
@@ -20,4 +24,10 @@ class DiceLoss(Function):
         intersect = torch.dot(input, target)
         num = (torch.mul(target, union) - 2*torch.mul(input, intersect))
         denom = (union*union)
-        out = 2*(num/denom)
+        grad_input = 2*(num/denom)
+        grad_output_expanded = grad_output.view(*repeat(1, grad_input.dim()))
+        grad_input.mul_(grad_output_expanded.expand_as(grad_input))
+        return grad_input , None
+
+def dice_loss(input, target):
+    return DiceLoss()(input, target)
