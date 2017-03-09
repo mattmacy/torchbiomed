84cef0795910826e4d7132c57edc1d2a22e1bf5f
Matt Macy <mmacy@nextbsd.org>
2017-03-08 17:18:45 -0800

    generate separate test and train datasets

--- datasets/luna16.py
+++ datasets/luna16.py
@@ -4,27 +4,67 @@ import torch.utils.data as data
 import os
 import os.path
 
-def make_dataset(dir, images, targets):
+
+def make_dataset(dir, images, targets, seed, train):
     image_dict = npz_load(dir + "/" + images)
     label_dict = npz_load(dir + "/" + targets)
+
+    negatives = []
+    positives = list(label_dict.keys())
+    assert len(positives) > 5
 
     for key in image_dict.keys():
         image = normalize(image_dict[key])
         image_dict[key] = image
-    
+        if key not in label_dict.keys():
+            label_dict[key] = np.zeros(shape_max, dtype=np.int8)
+            negatives.append(key)
+    np.random.seed(seed)
+    positives = np.random.shuffle(positives)
+    negatives = np.random.shuffle(negatives)
+
+    neg_test_count = 0
+    if len(negatives) > 5:
+        neg_test_count = len(negatives) // 5
+    pos_test_count = len(positives) // 5
+
+    train_series = []
+    test_series = []
+    count = 0
+    for series in positives:
+        if count < pos_test_count:
+            test_series.append(series)
+        else:
+            train_series.append(series)
+        count += 1
+    count = 0
+    for key, value in negatives:
+        if count < pos_test_count:
+            test_series.append(series)
+        else:
+            train_series.append(series)
+        count += 1
+
+    if train:
+        image_series = np.random.shuffle(train_series)
+    else:
+        image_series = np.random.shuffle(test_series)
     images = []
-    for key in label_dict.keys():
-        label = label_dict[key]
-        image = image_dict[key]
-        images.append((key, image, label))
+    for series in image_series:
+        label = label_dict[series]
+        image = image_dict[series]
+        images.append((series, image, label))
     return images
 
+
 class LUNA16(data.Dataset):
-    def __init__(self, root, images, targets, transform=None, target_transform=None,
-                 co_transform=None):
-        imgs = make_dataset(root, images, targets)
+    def __init__(self, root, images, targets, transform=None,
+                 target_transform=None, co_transform=None,
+                 train=True, seed=1):
+        imgs = make_dataset(root, images, targets, seed, )
         if len(imgs) == 0:
-            raise(RuntimeError("Found 0 targets: " + root + "/" + targets + "\n"))
+            raise(RuntimeError("Found 0 targets: " + root +
+                               "/" + targets + "\n"))
 
         self.root = root
         self.imgs = imgs
