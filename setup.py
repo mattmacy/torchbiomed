0f99fdb5277cd7d776f3d628b1e7d7750c05f10f
Matt Macy <mmacy@nextbsd.org>
2017-03-09 16:08:08 -0800

    add basics for packaging

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os
+import shutil
+import sys
+from setuptools import setup, find_packages
+
+
+readme = open('README.m').read()
+
+VERSION = '0.0.1'
+
+# same as ./requirements.txt
+requirements = [
+    'numpy',
+    'torch',
+    'torchvision',
+]
+
+setup(
+    # Metadata
+    name='torchbiomed'
+    version=VERSION,
+    author='Matthew Macy'
+    author_email='mat.macy@gmail.com'
+    url='https://github.com/mattmacy/torchbiomed',
+    description='biomedical image datasets, transforms, utilities, and models for torch deep learning',
+    long_description=readme,
+    license='BSD',
+
+    # Package info
+    packages=find_packages(exclude=('test',)),
