ad4b19038be09eae779810130d112b1b1aeeaff0
Matt Macy <mmacy@nextbsd.org>
2017-03-09 21:19:05 -0800

    generate train / test split once, only return train or test results from make_dataset

--- torchbiomed/datasets/luna16.py
+++ torchbiomed/datasets/luna16.py
@@ -1,64 +1,77 @@
 import numpy as np
+
+import torch
 import torch.utils.data as data
 import torchbiomed.utils as utils
 
 import os
 import os.path
+from torchbiomed.utils import Z_MAX, Y_MAX, X_MAX
 
+image_dict = {}
+label_dict = {}
+test_split = []
+train_split = []
+
+def train_test_split(full, positive):
+    negative = full - positive
+    test_neg_count = (len(negative) // 5) + 1
+    test_pos_count = (len(positive) // 5) + 1
+    negative_list = list(negative)
+    positive_list = list(positive)
+    np.random.shuffle(positive_list)
+    np.random.shuffle(negative_list)
+    test_negative = set()
+    test_positive = set()
+    for i in range(test_neg_count):
+        test_negative |= set([negative_list[i]])
+    for i in range(test_pos_count):
+        test_positive |= set([positive_list[i]])
+    train_negative = negative - test_negative
+    train_positive = positive - test_positive
+    train = list(train_positive | train_negative)
+    test = list(test_positive | test_negative)
+    np.random.shuffle(train)
+    np.random.shuffle(test)
+    return (train, test)
 
 def make_dataset(dir, images, targets, seed, train):
-    image_dict = utils.npz_load(dir + "/" + images)
-    label_dict = utils.npz_load(dir + "/" + targets)
+    global image_dict, label_dict, test_split, train_split
+    if len(image_dict) == 0:
+        image_dict = utils.npz_load(dir + "/" + images)
+    if len(label_dict) == 0:
+        label_dict = utils.npz_load(dir + "/" + targets)
 
-    negatives = []
-    positives = np.array(list(label_dict.keys()))
+    np.random.seed(seed)
+    positives = set(label_dict.keys())
+
     assert len(positives) > 5
 
-    for key in image_dict.keys():
-        image = utils.normalize(image_dict[key])
-        image_dict[key] = image
-        if key not in label_dict.keys():
-            label_dict[key] = np.zeros(utils.shape_max, dtype=np.int8)
-            negatives.append(key)
-    np.random.seed(seed)
-    negatives = np.array(negatives)
-    np.random.shuffle(positives)
-    if len(negatives) > 5:
-        np.random.shuffle(negatives)
-        neg_test_count = len(negatives) // 5
-
-    neg_test_count = 0
-    pos_test_count = len(positives) // 5
-
-    train_series = []
-    test_series = []
-    count = 0
-    for series in positives:
-        if count < pos_test_count:
-            test_series.append(series)
-        else:
-            train_series.append(series)
-        count += 1
-    count = 0
-    for series in negatives:
-        if count < pos_test_count:
-            test_series.append(series)
-        else:
-            train_series.append(series)
-        count += 1
-
+    full = set(image_dict.keys())
+    if len(test_split) == 0:
+        train_split, test_split = train_test_split(full, positives)
     if train:
-        np.random.shuffle(train_series)
-        image_series = train_series
+        keys = train_split
     else:
-        np.random.shuffle(test_series)
-        image_series = test_series
+        keys = test_split
+
+    labels = []
     images = []
-    for series in image_series:
-        label = label_dict[series]
-        image = image_dict[series]
-        images.append((series, image, label))
-    return images
+    zero_tensor = torch.FloatTensor(Z_MAX, Y_MAX, X_MAX).zero_()
+    for key in keys:
+        if key not in label_dict.keys():
+            labels.append(zero_tensor)
+        else:
+            label = np.array(label_dict[key], dtype=np.float32)
+            #copy_tensor(label_tensor, label_dict[key])
+            labels.append(torch.Tensor(label))
+
+    for key in keys:
+        image = image_dict[key]
+        images.append(torch.Tensor(utils.normalize(image)))
+
+    results = list(zip(images, labels))
+    return results
 
 
 class LUNA16(data.Dataset):
@@ -80,7 +93,7 @@ class LUNA16(data.Dataset):
         self.co_transform = co_transform
 
     def __getitem__(self, index):
-        _, img, target = self.imgs[index]
+        img, target = self.imgs[index]
         if self.transform is not None:
             img = self.transform(img)
         if self.target_transform is not None:
