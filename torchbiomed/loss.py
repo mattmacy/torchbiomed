c48b8d023214a5d6594884bbef2c1d9efb293302
Matt Macy <mmacy@nextbsd.org>
2017-03-13 00:15:29 -0700

    switch to channel one hot encoding as in caffe vnet

--- torchbiomed/loss.py
+++ torchbiomed/loss.py
@@ -1,6 +1,7 @@
 import torch
 from torch.autograd import Function
 from itertools import repeat
+import numpy as np
 
 # Intersection = dot(A, B)
 # Union = dot(A, A) + dot(B, B)
@@ -16,27 +17,37 @@ class DiceLoss(Function):
     def forward(self, input, target):
         self.save_for_backward(input, target)
         target = target.view(1, target.numel())
-        # target is a binary valued tensor - dot(target, target) => sum(target)
-        input = input[0]
-        intersect = torch.dot(input, target)
-        union = torch.dot(input, input) + torch.sum(target)
-        IoU = intersect/ union
-#        print('union: {:.3f}\t intersect: {:.6f}\t IoU: {:.7f}'.format(
-#            union, intersect, IoU))
-        out = torch.FloatTensor(1).fill_(0.)
-        out = torch.add(out, 2*IoU)
+        eps = 0.0000001
+        _, result_ = input.max(0)
+        result_ = torch.squeeze(result_)
+        result = torch.cuda.FloatTensor(result_.size())
+        result.copy_(result_)
+
+        intersect = torch.dot(result, target)
+        # binary values so sum the same as sum of squares
+        result_sum = torch.sum(result)
+        target_sum = torch.sum(target)
+        union = result_sum + target_sum + (2*eps)
+
+        # the target volume can be empty - so we still want to
+        # end up with a score of 1 if the result is 0/0
+        IoU = intersect / union
+#        print('union: {:.3f}\t intersect: {:.6f}\t target_sum: {:.0f} IoU: result_sum: {:.0f} IoU {:.7f}'.format(
+#            union, intersect, target_sum, result_sum, 2*IoU))
+        out = torch.FloatTensor(1).fill_(2*IoU)
+        self.intersect, self.union = intersect, union
         return out
 
     def backward(self, grad_output):
         input, target = self.saved_tensors
+        intersect, union = self.intersect, self.union
         target = target.view(1, target.numel())
-        union = torch.dot(input[0], input[0]) +  torch.sum(target)
-        intersect = torch.dot(input[0], target)
-        num = (torch.mul(target, union) - 2*torch.mul(input[0], intersect))
-        denom = (union*union)
-
-        grad_input = torch.cat((torch.mul(torch.div(num,denom), 2*grad_output[0]),
-                                torch.mul(torch.div(num,denom), -2*grad_output[0])), 0)
+        gt = torch.div(target, union)
+        IoU2 = intersect/(union*union)
+        pred = torch.mul(input[1], IoU2)
+        dDice = torch.add(torch.mul(gt, 2), torch.mul(pred, -4))
+        grad_input = torch.cat((torch.mul(dDice, grad_output[0]),
+                                torch.mul(dDice, -grad_output[0])), 0)
         return grad_input , None
 
 def dice_loss(input, target):
