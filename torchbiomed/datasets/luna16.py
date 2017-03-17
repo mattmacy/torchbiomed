13a8b340fe5e17958a672fcaa6cc8117138751b2
Matt Macy <mmacy@nextbsd.org>
2017-03-16 23:26:54 -0700

    checkpoint nodule mask generation support

--- torchbiomed/datasets/luna16.py
+++ torchbiomed/datasets/luna16.py
@@ -7,6 +7,7 @@ from glob import glob
 import os
 import os.path
 import SimpleITK as sitk
+import pandas as pd
 
 MIN_BOUND = -1000
 MAX_BOUND = 400
@@ -178,6 +179,139 @@ def normalize_lung_mask(**kwargs):
         img, _, _ = utils.resample_volume(img_array, spacing_old, img_spacing)
         utils.save_updated_image(img, itk_img, luna_seg_lungs_save_path+os.path.basename(img_file), img_spacing)
 
+
+def normalize_nodule_mask(**kwargs):
+    luna_path = kwargs['orig']
+    file_list = glob(luna_path+"*.mhd")
+    pixel_count = 0
+    mask_count = 0
+    annotations = kwargs['annotations']
+    Z_MAX, Y_MAX, X_MAX = kwargs['Z_MAX'], kwargs['Y_MAX'], kwargs['X_MAX']
+    shape_max = (Z_MAX, Y_MAX, X_MAX)
+    vox_spacing = kwargs['vox_spacing']
+    utils.init_dims3D(Z_MAX, Y_MAX, X_MAX, vox_spacing)
+    x_list, y_list, z_list = [], [], []
+
+    def get_boundaries(origin, offsets, params):
+        diam, center = params
+        diam3 = np.array((diam, diam, diam))
+        diamu = diam + vox_spacing
+        diam3u = np.array((diamu, diamu, diamu))
+        v_center = np.rint((center - origin)/vox_spacing)
+        v_lower = np.rint((center - diam3 - origin)/vox_spacing)
+        v_upper = np.rint((center + diam3u - origin)/vox_spacing)
+        v_center -= offsets
+        v_lower -= offsets
+        v_upper -= offsets
+        #print((v_lower, v_center, v_upper))
+        #vox_check(v_lower)
+        #vox_check(v_center)
+        #vox_check(v_upper)
+        x_list.append(v_upper[2])
+        y_list.append(v_upper[1])
+        z_list.append(v_upper[0])
+        x_list.append(v_lower[2])
+        y_list.append(v_lower[1])
+        z_list.append(v_lower[0])
+        return (v_lower, v_center, v_upper)
+
+    def l2_norm(pointA, pointB):
+        point = pointA - pointB
+        return np.sqrt(np.dot(point, point))
+
+    def get_filename(case):
+        for f in file_list:
+            if case in f:
+                return(f)
+
+    def update_mask(mask, CT, bounds):
+        v_lower, v_center, v_upper = bounds
+        z_min, y_min, x_min = v_lower
+        z_max, y_max, x_max = v_upper
+        pixel_count = 0
+        radius = np.rint((z_max - z_min + vox_spacing)/2)
+        ct_thresh = MIN_BOUND + 1
+        for z in range(z_min, z_max):
+            for y in range(y_min, y_max):
+                for x in range(x_min, x_max):
+                    vox = np.array((z, y, x))
+                    off = vox - v_center
+                    if l2_norm(off) > radius:
+                        break
+                    if CT[z][y][x] > ct_thresh:
+                        mask[z][y][x] = 5
+                        pixel_count += 1
+                        bit_count += 1
+        assert bit_count != 0
+
+    origin_dict = {}
+    offset_dict = {}
+    luna_normal_path = kwargs['src']
+    luna_mask_path = kwargs['dst']
+
+    count = 0
+    for img_file in file_list:
+        series = os.path.basename(img_file)[:-4]
+        itk_img = sitk.ReadImage(img_file)
+        (x_space, y_space, z_space) = itk_img.GetSpacing()
+        img_spacing = (z_space, y_space, x_space)
+        x_size, y_size, z_size = itk_img.GetSize()
+        img_size = (z_size, y_size, x_size)
+        resize_factor = np.array(img_spacing) / [vox_spacing, vox_spacing, vox_spacing]
+        (new_z, new_y, new_x) = np.round(img_size * resize_factor)
+        z_off = int((new_z - Z_MAX)/2)
+        y_off = int((new_y - Y_MAX)/2)
+        x_off = int((new_x - X_MAX)/2)
+        offset_dict[series] = np.array((z_off, y_off, x_off))
+        origin = np.array(itk_img.GetOrigin())
+        if origin[1] < 0 and origin[2] < 0:
+            origin[1] = -origin[1]
+            origin[2] = -origin[2]
+        origin_dict[series] = origin
+#        if count == 100:
+#            break
+#        count += 1
+
+    file_list=glob(luna_normal_path+"*.mhd")
+
+    df_node = pd.read_csv(annotations)
+    df_node["file"] = df_node["seriesuid"].apply(get_filename)
+    df_node = df_node.dropna()
+    img_spacing = (vox_spacing, vox_spacing, vox_spacing)
+    count = 0
+    for img_file in file_list:
+        mask_count = 0
+        mini_df = df_node[df_node["file"]==img_file] #get all nodules associate with file
+        if len(mini_df) == 0:
+            continue
+        mask = np.full(shape_max, -1024, dtype=np.int16)
+        series = os.path.basename(img_file)[0:-4]
+        origin = origin_dict[series]
+        offsets = offset_dict[series]
+        itk_img = sitk.ReadImage(img_file)
+        img_array = sitk.GetArrayFromImage(itk_img)
+        for i in range(len(mini_df)):
+            node_x = mini_df["coordX"].values[i]
+            node_y = mini_df["coordY"].values[i]
+            node_z = mini_df["coordZ"].values[i]
+            diam = mini_df["diameter_mm"].values[i]
+            params = (diam, np.array((node_z, node_y, node_x)))
+            bounds = get_boundaries(origin, offsets, params)
+            _, v_center, _ = bounds
+            if np.min(v_center) < 0:
+                continue
+            # XXX check this
+            bounds = np.clip(bounds, 0, Z_MAX).astype(np.int16)
+            update_mask(mask, img_array, bounds)
+            mask_count += 1
+        assert mask_count != 0
+        itk_mask_img = sitk.GetImageFromArray(mask, isVector=False)
+        itk_mask_img.SetSpacing(img_spacing)
+        itk_mask_img.SetOrigin(origin)
+        sitk.WriteImage(itk_mask_img, luna_mask_path+'/'+os.path.basename(img_file))
+#        if count == 10:
+#            break
+#        count += 1
 
 class LUNA16(data.Dataset):
     def __init__(self, root='.', images=None, targets=None, transform=None,
