208da10b194e77ff5c2edc55c5e298a77224cadf
Matt Macy <mmacy@nextbsd.org>
2017-03-24 11:40:57 -0700

    add production inference support

--- torchbiomed/datasets/luna16.py
+++ torchbiomed/datasets/luna16.py
@@ -14,13 +14,16 @@ MAX_BOUND = 400
 
 image_dict = {}
 label_dict = {}
+mask_dict = {}
+stats_dict = {}
 test_split = []
 train_split = []
 
-def train_test_split(full, positive):
+
+def train_test_split(full, positive, test_fraction):
     negative = full - positive
-    test_neg_count = (len(negative) // 5) + 1
-    test_pos_count = (len(positive) // 5) + 1
+    test_neg_count = int(np.ceil(len(negative)*test_fraction))
+    test_pos_count = int(np.ceil(len(positive)*test_fraction))
     negative_list = list(negative)
     positive_list = list(positive)
     np.random.shuffle(positive_list)
@@ -43,54 +46,70 @@ def train_test_split(full, positive):
     np.random.shuffle(test)
     return (train, test)
 
+
 def load_image(root, series):
     if series in image_dict.keys():
         return image_dict[series]
-    img_file = root + "/" + series + ".mhd"
+    img_file = os.path.join(root, series + ".mhd")
     itk_img = sitk.ReadImage(img_file)
     img = sitk.GetArrayFromImage(itk_img)
     z, y, x = np.shape(img)
     img = img.reshape((1, z, y, x))
     image_dict[series] = utils.truncate(img, MIN_BOUND, MAX_BOUND)
+    stats_dict[series] = itk_img.GetOrigin(), itk_img.GetSpacing()
     return img
+
 
 def load_label(root, series):
     if series in label_dict.keys():
         return label_dict[series]
-    img_file = root + "/" + series + ".mhd"
+    img_file = os.path.join(root, series + ".mhd")
     itk_img = sitk.ReadImage(img_file)
     img = sitk.GetArrayFromImage(itk_img).astype(np.uint8)
     img[img != 0] = 1
     label_dict[series] = img
     return img
 
-def make_dataset(dir, images, targets, seed, train, allow_empty, class_balance, partition):
+
+def load_mask(root, series):
+    if series in mask_dict.keys():
+        return mask_dict[series]
+    img_file = os.path.join(root, series + ".mhd")
+    itk_img = sitk.ReadImage(img_file)
+    img = sitk.GetArrayFromImage(itk_img)
+    img[img != 0] = 1
+    mask_dict[series] = img
+    return img
+
+def full_dataset(dir, images):
+    image_path = os.path.join(dir, images)
+    image_files = glob(os.path.join(image_path, "*.mhd"))
+    image_list = []
+    for name in image_files:
+        image_list.append(os.path.basename(name)[:-4])
+    return image_list
+
+def make_dataset(dir, images, targets, seed, train, class_balance, partition, nonempty, test_fraction, mode):
     global image_dict, label_dict, test_split, train_split
     zero_tensor = None
 
-    label_path = dir + "/" + targets
-    label_files = glob(label_path + "/*.mhd")
+    assert mode != "infer"
+    label_path = os.path.join(dir, targets)
+    label_files = glob(os.path.join(label_path, "*.mhd"))
     label_list = []
     for name in label_files:
         label_list.append(os.path.basename(name)[:-4])
-
-    target_weight = 0
-    if class_balance:
-        for series in label_list:
-            label = load_label(label_path, series)
-            target_weight += np.mean(label)
-        target_weight /= len(label_list)
 
     sample_label = load_label(label_path, label_list[0])
     shape = np.shape(sample_label)
     if len(test_split) == 0:
         zero_tensor = np.zeros(shape, dtype=np.uint8)
         image_list = []
-        image_path = dir + "/" + images
+        image_path = os.path.join(dir, images)
         file_list=glob(image_path + "/*.mhd")
         for img_file in file_list:
             series = os.path.basename(img_file)[:-4]
-            if not allow_empty and series not in label_list:
+            if series not in label_list:
                 continue
             image_list.append(series)
             if series not in label_list:
@@ -123,13 +142,21 @@ def make_dataset(dir, images, targets, seed, train, allow_empty, class_balance, 
                     xend = xstart + x_incr
                     part_list.append(((zstart, zend), (ystart, yend), (xstart, xend)))
     else:
-        part_list = ((0, z), (0, y), (0, x))
+        part_list = [((0, z), (0, y), (0, x))]
     result = []
+    target_means = []
     for key in keys:
         for part in part_list:
+            target = load_label(label_path, key)
+            if nonempty:
+                if np.sum(utils.get_subvolume(target, part)) == 0:
+                    continue
+            target_means.append(np.mean(target))
             result.append((key, part))
 
-    return (result, target_weight)
+    target_mean = np.mean(target_means)
+    return (result, target_mean)
+
 
 def normalize_lung_CT(**kwargs):
     mean_values = []
@@ -141,7 +168,7 @@ def normalize_lung_CT(**kwargs):
     utils.init_dims3D(Z_MAX, Y_MAX, X_MAX, vox_spacing)
     luna_subset_path = kwargs['src']
     luna_save_path = kwargs['dst']
-    file_list=glob(luna_subset_path+"*.mhd")
+    file_list=glob(luna_subset_path + "/" + "*.mhd")
     img_spacing = (vox_spacing, vox_spacing, vox_spacing)
 
     for img_file in file_list:
@@ -150,7 +177,7 @@ def normalize_lung_CT(**kwargs):
         spacing_old = (z_space, y_space, x_space)
         img_array = sitk.GetArrayFromImage(itk_img) # indexes are z,y,x (notice the ordering)
         img, mu, var = utils.resample_volume(img_array, spacing_old, img_spacing, bounds=(MIN_BOUND, MAX_BOUND))
-        utils.save_updated_image(img, itk_img, luna_save_path+os.path.basename(img_file), img_spacing)
+        utils.save_updated_image(img, luna_save_path+os.path.basename(img_file), itk_img.GetOrigin(), img_spacing)
         mean_values.append(mu)
         var_values.append(var)
     dataset_mean = np.mean(mean_values)
@@ -164,7 +191,7 @@ def normalize_lung_mask(**kwargs):
     utils.init_dims3D(Z_MAX, Y_MAX, X_MAX, vox_spacing)
     luna_seg_lungs_path = kwargs['src']
     luna_seg_lungs_save_path = kwargs['dst']
-    file_list=glob(luna_seg_lungs_path + "/" +"*.mhd")
+    file_list=glob(os.path.join(luna_seg_lungs_path, "*.mhd"))
     img_spacing = (vox_spacing, vox_spacing, vox_spacing)
     for img_file in file_list:
         itk_img = sitk.ReadImage(img_file)
@@ -173,14 +200,15 @@ def normalize_lung_mask(**kwargs):
         img_array = sitk.GetArrayFromImage(itk_img) # indexes are z,y,x (notice the ordering)
         img, _, _ = utils.resample_volume(img_array, spacing_old, img_spacing)
         img[img < 1] = 0
-        utils.save_updated_image(img, itk_img,
-                                 os.path.join(luna_seg_lungs_save_path, os.path.basename(img_file)),
-                                 img_spacing)
+        utils.save_updated_image(img, os.path.join(luna_seg_lungs_save_path, os.path.basename(img_file)),
+                                 itk_img.GetOrigin(), img_spacing)
 
 
-def build_nodule_offset_tables(file_list, save_path):
+def build_nodule_offset_tables(file_list, save_path, **kwargs):
     offset_dict = {}
     origin_dict = {}
+    X_MAX, Y_MAX, Z_MAX = kwargs['X_MAX'], kwargs['Y_MAX'], kwargs['Z_MAX']
+    vox_spacing = kwargs['vox_spacing']
     for img_file in file_list:
         series = os.path.basename(img_file)[:-4]
         itk_img = sitk.ReadImage(img_file)
@@ -247,9 +275,8 @@ def normalize_nodule_mask(**kwargs):
         z_min, y_min, x_min = v_lower
         z_max, y_max, x_max = v_upper
         pixel_count = 0
-        min_ct = np.min(CT)
         radius = np.rint((z_max - z_min + vox_spacing)/2)
-        ct_thresh = min_ct + 1
+        ct_thresh = -1000
         bit_count = 0
         #print(v_center)
         for z in range(z_min, z_max):
@@ -261,7 +288,9 @@ def normalize_nodule_mask(**kwargs):
                         mask[z][y][x] = 4
                         pixel_count += 1
                         bit_count += 1
-        assert bit_count != 0
+        #assert bit_count != 0
+        if bit_count == 0:
+            print(v_center)
 
     origin_dict = utils.npz_load(os.path.join(tables_path, "origin_table"))
     offset_dict = utils.npz_load(os.path.join(tables_path, "offset_table"))
@@ -298,7 +327,7 @@ def normalize_nodule_mask(**kwargs):
                 print("origin: {} offsets: {}\n params: {} v_center: {}".format(
                     origin, offsets, params, v_center))
                 continue
-            bounds = np.array(bounds.astype(np.int16)
+            bounds = np.array(bounds).astype(np.int16)
             update_mask(mask, img_array, bounds)
             mask_count += 1
         assert mask_count != 0
@@ -310,29 +339,60 @@ def normalize_nodule_mask(**kwargs):
 
 class LUNA16(data.Dataset):
     def __init__(self, root='.', images=None, targets=None, transform=None,
-                 target_transform=None, co_transform=None,
-                 train=True, seed=1, allow_empty=True, class_balance=False, split=None):
-        if images is None or targets is None:
-            raise(RuntimeError("both images and targets must be set"))
-
-        imgs, target_weight = make_dataset(root, images, targets, seed, train, allow_empty, class_balance, split)
+                 target_transform=None, co_transform=None, mode="train", seed=1,
+                 class_balance=False, split=None, masks=None, nonempty=True,
+                 test_fraction=0.15):
+        if images is None:
+            raise(RuntimeError("images must be set"))
+        if targets is None and mode != "infer":
+            raise(RuntimeError("both images and targets must be set if mode is not 'infer'"))
+        if mode == "infer":
+            imgs = full_dataset(root, images)
+        else:
+            imgs, target_mean = make_dataset(root, images, targets, seed, mode, class_balance, split, nonempty, test_fraction)
+            self.data_mean = target_mean
         if len(imgs) == 0:
-            raise(RuntimeError("Found 0 targets: " + root +
-                               "/" + targets + "\n"))
+            raise(RuntimeError("Found 0 images: " + os.path.join(root, images) + "\n"))
 
-        self.fg_weight = target_weight
+        self.mode = mode
         self.root = root
         self.imgs = imgs
-        self.targets = self.root + "/" + targets
-        self.images = self.root + "/" + images
+        self.masks = None
+        self.split = split
+        if masks is not None:
+            self.masks = os.path.join(self.root, masks)
+        if targets is not None:
+            self.targets = os.path.join(self.root, targets)
+        self.images = os.path.join(self.root, images)
         self.transform = transform
         self.target_transform = target_transform
         self.co_transform = co_transform
 
-    def target_weight(self):
-        return self.fg_weight
+    def target_mean(self):
+        return self.data_mean
 
     def __getitem__(self, index):
+        if self.mode == "train" or self.mode == "test":
+            return self.__getitem_dev(index)
+        elif self.mode == "infer":
+            return self.__getitem_prod(index)
+
+    def __getitem_prod(self, index):
+        series = self.imgs[index]
+        image = load_image(self.images, series)
+        origin, spacing = stats_dict[series]
+        image = image.astype(np.float32)
+        if self.split is not None:
+            batches = utils.partition_image(image, self.split)
+        else:
+            batches = [image]
+        if self.transform is not None:
+            batches = map(self.transform, batches)
+            batches = [*batches]
+        batches = torch.cat(batches)
+        return batches, series, origin, spacing
+
+    def __getitem_dev(self, index):
         series, bounds = self.imgs[index]
         (zs, ze), (ys, ye), (xs, xe) = bounds
         target = load_label(self.targets, series)
@@ -340,6 +400,13 @@ class LUNA16(data.Dataset):
         target = torch.from_numpy(target.astype(np.int64))
         image = load_image(self.images, series)
         image = image[0, zs:ze, ys:ye, xs:xe]
+        # optionally mask out lung volume from image
+        if self.masks is not None:
+            mask = load_mask(self.masks, series)
+            mask = mask[zs:ze, ys:ye, xs:xe]
+            image -= MIN_BOUND
+            image = np.multiply(mask, image)
+            image += MIN_BOUND
         image = image.reshape((1, ze-zs, ye-ys, xe-xs))
         img = image.astype(np.float32)
 
